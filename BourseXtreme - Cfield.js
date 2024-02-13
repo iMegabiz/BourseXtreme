12d284ed6a5095e486eb59d41f4eb8ed8cb055b0
iMegabiz <159781143+iMegabiz@users.noreply.github.com>
2024-02-13 19:35:50 +0330

    Add files via upload

---
+++ BourseXtreme - Cfield.js
@@ -0,0 +1,113 @@
+true === function()
+{
+	var C0 = "cfield0"
+		, C1 = "cfield1"
+		, C2 = "cfield2"
+		, C3 = "cfield3"
+		, C4 = "cfield4"
+		, C5 = "cfield5"
+		, C6 = "cfield6"
+		, C7 = "cfield7"
+		, C8 = "cfield8"
+		, C9 = "cfield9"
+		, C10 = "cfield10"
+		, C11 = "cfield11"
+		, C12 = "cfield12"
+		, C13 = "cfield13"
+		, C14 = "cfield14"
+		, C15 = "cfield15"
+		, C16 = "cfield16"
+		, C17 = "cfield17"
+		, C18 = "cfield18"
+		, C19 = "cfield19";
+	var my_data_title0 = '<div style = "direction:rtl;";><span style = "border: 1px solid gray;width: 10%;display: inline-block;text-align: center;color: #000;background: #D9D9D9;font-weight: normal;"> ' + C0 + '</span>'
+	my_data_title0 += '<span style = "border: 1px solid gray;width: 10%;display: inline-block;text-align: center;color: #000;background: #D9D9D9;font-weight: normal;"> ' + C1 + '</span>'
+	my_data_title0 += '<span style = "border: 1px solid gray;width: 10%;display: inline-block;text-align: center;color: #000;background: #D9D9D9;font-weight: normal;"> ' + C2 + '</span>'
+	my_data_title0 += '<span style = "border: 1px solid gray;width: 10%;display: inline-block;text-align: center;color: #000;background: #D9D9D9;font-weight: normal;"> ' + C3 + '</span>'
+	my_data_title0 += '<span style = "border: 1px solid gray;width: 10%;display: inline-block;text-align: center;color: #000;background: #D9D9D9;font-weight: normal;"> ' + C4 + '</span>'
+	my_data_title0 += '<span style = "border: 1px solid gray;width: 10%;display: inline-block;text-align: center;color: #000;background: #D9D9D9;font-weight: normal;"> ' + C5 + '</span>'
+	my_data_title0 += '<span style = "border: 1px solid gray;width: 10%;display: inline-block;text-align: center;color: #000;background: #D9D9D9;font-weight: normal;"> ' + C6 + '</span>'
+	my_data_title0 += '<span style = "border: 1px solid gray;width: 10%;display: inline-block;text-align: center;color: #000;background: #D9D9D9;font-weight: normal;"> ' + C7 + '</span>'
+	my_data_title0 += '<span style = "border: 1px solid gray;width: 10%;display: inline-block;text-align: center;color: #000;background: #D9D9D9;font-weight: normal;"> ' + C8 + '</span>'
+	my_data_title0 += '<span style = "border: 1px solid gray;width: 8%;display: inline-block;text-align: center;color: #000;background: #D9D9D9;font-weight: normal;"> ' + C9 + '</span></div>'
+	var my_data_title1 = '<div style = "direction:rtl;";><span style = "border: 1px solid gray;width: 10%;display: inline-block;text-align: center;color: #000;background: #D9D9D9;font-weight: normal;"> ' + C10 + '</span>'
+	my_data_title1 += '<span style = "border: 1px solid gray;width: 10%;display: inline-block;text-align: center;color: #000;background: #D9D9D9;font-weight: normal;"> ' + C11 + '</span>'
+	my_data_title1 += '<span style = "border: 1px solid gray;width: 10%;display: inline-block;text-align: center;color: #000;background: #D9D9D9;font-weight: normal;"> ' + C12 + '</span>'
+	my_data_title1 += '<span style = "border: 1px solid gray;width: 10%;display: inline-block;text-align: center;color: #000;background: #D9D9D9;font-weight: normal;"> ' + C13 + '</span>'
+	my_data_title1 += '<span style = "border: 1px solid gray;width: 10%;display: inline-block;text-align: center;color: #000;background: #D9D9D9;font-weight: normal;"> ' + C14 + '</span>'
+	my_data_title1 += '<span style = "border: 1px solid gray;width: 10%;display: inline-block;text-align: center;color: #000;background: #D9D9D9;font-weight: normal;"> ' + C15 + '</span>'
+	my_data_title1 += '<span style = "border: 1px solid gray;width: 10%;display: inline-block;text-align: center;color: #000;background: #D9D9D9;font-weight: normal;"> ' + C16 + '</span>'
+	my_data_title1 += '<span style = "border: 1px solid gray;width: 10%;display: inline-block;text-align: center;color: #000;background: #D9D9D9;font-weight: normal;"> ' + C17 + '</span>'
+	my_data_title1 += '<span style = "border: 1px solid gray;width: 10%;display: inline-block;text-align: center;color: #000;background: #D9D9D9;font-weight: normal;"> ' + C18 + '</span>'
+	my_data_title1 += '<span style = "border: 1px solid gray;width: 10%;display: inline-block;text-align: center;color: #000;background: #D9D9D9;font-weight: normal;"> ' + C19 + '</span></div>'
+	my_data_title1 += '<span style = "left: 10px;direction: rtl; display: inline-block; background: #F6F7BF;font-weight: normal; text-align: center;border: 0 solid red;border-radius: 6px; padding: 3px;top: 50%;position: fixed;font-family: Tahoma; font-size: 12px; background-color: #ff5;  box-shadow: 0 2px 5px rgb(0 0 0 / 50%);color: Blue; width: 236px;animation: NotificationAnim 2s infinite linear;"><a href="https://t.me/BourseXtreme" title="کانال تلگرام BourseXtreme" target="_blank">Source Code By: @BourseXtreme</a></span>'
+	my_data_title1 += '<span style="left: 10px;direction: rtl;display: inline-block;background: #F6F7BF;font-weight: normal;text-align: center;border: 0 solid red;border-radius: 6px;padding: 3px;top: 55%;position: fixed;font-family: Tahoma;font-size: 12px;background-color: #ff5;box-shadow: 0 2px 5px rgb(0 0 0 / 50%);color: Blue;width: 236px;animation: NotificationAnim 2s infinite linear;">آموزش‌های رایگان ما را <br> در کانال تلگرام <a href="https://t.me/BourseXtreme" title="کانال تلگرام BourseXtreme" target="_blank"><span style="font-weight: bold;color: red;">BourseXtreme</span></a> دنبال کنید.</span></div>'
+	var my_data_title2 = ""
+	var cfield0_title = my_data_title0
+		, cfield1_title = my_data_title1
+		, cfield2_title = my_data_title2;
+	var wc0 = "1200px"
+		, wc1 = "1200px"
+		, wc2 = "1px";
+	var title = document.getElementsByClassName("t0head");
+	for (var i = 0; i < title.length; i++)
+	{
+		if (title[i].outerHTML.includes("cfield0"))
+		{
+			title[i].innerHTML = cfield0_title;
+			title[i].style.width = wc0;
+			title[i].style.fontWeight = "normal";
+		}
+		else if (title[i].outerHTML.includes("cfield1"))
+		{
+			title[i].innerHTML = cfield1_title;
+			title[i].style.width = wc1;
+			title[i].style.fontWeight = "normal";
+		}
+		else if (title[i].outerHTML.includes("cfield2"))
+		{
+			title[i].innerHTML = cfield2_title;
+			title[i].style.width = wc2;
+			title[i].style.fontWeight = "normal";
+		}
+	}
+	var c0 = document.getElementsByClassName("ch{_cfield0}");
+	var c1 = document.getElementsByClassName("ch{_cfield1}");
+	var c2 = document.getElementsByClassName("ch{_cfield2}");
+	for (var i = 0; i < c0.length; i++)
+	{
+		c0[i].style.width = wc0;
+		c1[i].style.width = wc1;
+		c2[i].style.width = wc2;
+	}
+	var my_data = '<div style = "direction:rtl;";><span style = "border: 1px solid gray;width: 10%;display: inline-block;color: #000;background: #FFFFFF;font-weight: normal;direction: ltr;"> ' + (pl) + '</span>'
+	my_data += '<span style = "border: 1px solid gray;width: 10%;display: inline-block;color: #000;background: #FFFFFF;font-weight: normal;direction: ltr;"> ' + (pc) + '</span>'
+	my_data += '<span style = "border: 1px solid gray;width: 10%;display: inline-block;color: #000;background: #FFFFFF;font-weight: normal;direction: ltr;"> ' + "2" + '</span>'
+	my_data += '<span style = "border: 1px solid gray;width: 10%;display: inline-block;color: #000;background: #FFFFFF;font-weight: normal;direction: ltr;"> ' + "3" + '</span>'
+	my_data += '<span style = "border: 1px solid gray;width: 10%;display: inline-block;color: #000;background: #FFFFFF;font-weight: normal;direction: ltr;"> ' + "4" + '</span>'
+	my_data += '<span style = "border: 1px solid gray;width: 10%;display: inline-block;color: #000;background: #FFFFFF;font-weight: normal;direction: ltr;"> ' + "5" + '</span>'
+	my_data += '<span style = "border: 1px solid gray;width: 10%;display: inline-block;color: #000;background: #FFFFFF;font-weight: normal;direction: ltr;"> ' + "6" + '</span>'
+	my_data += '<span style = "border: 1px solid gray;width: 10%;display: inline-block;color: #000;background: #FFFFFF;font-weight: normal;direction: ltr;"> ' + "7" + '</span>'
+	my_data += '<span style = "border: 1px solid gray;width: 10%;display: inline-block;color: #000;background: #FFFFFF;font-weight: normal;direction: ltr;"> ' + "8" + '</span>'
+	my_data += '<span style = "border: 1px solid gray;width: 8%;display: inline-block;color: #000;background: #FFFFFF;font-weight: normal;direction: ltr;"> ' + "9" + '</span></div>'
+	var my_data1 = '<div style = "direction:rtl;";><span style = "border: 1px solid gray;width: 10%;display: inline-block;text-align: center;color: #000;background: #FFFFFF;font-weight: normal;"> ' + "10" + '</span>'
+	my_data1 += '<span style = "border: 1px solid gray;width: 10%;display: inline-block;color: #000;background: #FFFFFF;font-weight: normal;direction: ltr;"> ' + "11" + '</span>'
+	my_data1 += '<span style = "border: 1px solid gray;width: 10%;display: inline-block;color: #000;background: #FFFFFF;font-weight: normal;direction: ltr;"> ' + "12" + '</span>'
+	my_data1 += '<span style = "border: 1px solid gray;width: 10%;display: inline-block;color: #000;background: #FFFFFF;font-weight: normal;direction: ltr;"> ' + "13" + '</span>'
+	my_data1 += '<span style = "border: 1px solid gray;width: 10%;display: inline-block;color: #000;background: #FFFFFF;font-weight: normal;direction: ltr;"> ' + "14" + '</span>'
+	my_data1 += '<span style = "border: 1px solid gray;width: 10%;display: inline-block;color: #000;background: #FFFFFF;font-weight: normal;direction: ltr;"> ' + "15" + '</span>'
+	my_data1 += '<span style = "border: 1px solid gray;width: 10%;display: inline-block;color: #000;background: #FFFFFF;font-weight: normal;direction: ltr;"> ' + "16" + '</span>'
+	my_data1 += '<span style = "border: 1px solid gray;width: 10%;display: inline-block;color: #000;background: #FFFFFF;font-weight: normal;direction: ltr;"> ' + "17" + '</span>'
+	my_data1 += '<span style = "border: 1px solid gray;width: 10%;display: inline-block;color: #000;background: #FFFFFF;font-weight: normal;direction: ltr;"> ' + "18" + '</span>'
+	my_data1 += '<span style = "border: 1px solid gray;width: 10%;display: inline-block;color: #000;background: #FFFFFF;font-weight: normal;direction: ltr;"> ' + "19" + '</span></div>'
+	var Symbol = ((l18))
+		.replaceAll("ك", "ک")
+		.replaceAll("ي", "ی");
+	if (Symbol == "خزامیا" || Symbol == "" || Symbol == "")
+	{
+		(cfield0) = my_data;
+		(cfield1) = my_data1;
+		(cfield2) = "";
+		return true;
+	}
+}();
